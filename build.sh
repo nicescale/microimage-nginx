93b89ea12a74c69b61efd8353ee6cfa674a1f365
shellrock <hanwoody@gmail.com>
2015-08-27 13:32:01 +0800

    auto build test

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ export CON_NAME=nginx_t
 export REG_URL=d.nicescale.com:5000
 export IMAGE=nginx
 export TAGS="1.8 1.8.1"
-export BASE_IMAGE=$REG_URL/microimages/alpine
+export BASE_IMAGE=microimages/alpine
 
-docker pull $BASE_IMAGE
+docker pull $REG_URL/$BASE_IMAGE
+
+docker tag -f $REG_URL/$BASE_IMAGE $BASE_IMAGE
 
 docker build -t $REG_URL/microimages/$IMAGE .
 
@@ -16,8 +18,8 @@ docker build -t $REG_URL/microimages/$IMAGE .
 
 echo "---> Starting push $REG_URL/microimages/$IMAGE:$VERSION"
 
-docker push $REG_URL/microimages/$IMAGE
 for t in $TAGS; do
   docker tag $REG_URL/microimages/$IMAGE $REG_URL/microimages/$IMAGE:$t
-  docker push $REG_URL/microimages/$IMAGE:$t
 done
+
+docker push $REG_URL/microimages/$IMAGE
